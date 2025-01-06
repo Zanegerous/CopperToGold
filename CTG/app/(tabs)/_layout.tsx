4878bfff02bf6d8ce248b863f351a7c874ff6f26
Zanegerous <155862535+Zanegerous@users.noreply.github.com>
2025-01-06 15:04:59 -0600

    Created navigation basic setup and enviroment

---
+++ CTG/app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{tabBarActiveTintColor: 'blue'}}>
+      <Tabs.Screen name="index" options={{headerShown: false, title: "Home"}} />
+      <Tabs.Screen name="settings" options={{headerShown: false, title: "Settings"}} />
+    </Tabs>
+  )
+
+
+
+}
---
+++ CTG/app/(tabs)/settings.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+export default function Settings(){
+    return(
+        <SafeAreaView>
+            <Text> Welcome to the settings page </Text>
+        </SafeAreaView>
+    )
+}
