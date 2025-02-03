0a4c5b234f5cea0303d8dc2d88eaf7ff1d00da75
Coby Taylor <117447744+KingOuroboros@users.noreply.github.com>
2025-02-03 14:01:02 -0600

    Updated

--- CTG/app/_layout.tsx
+++ CTG/app/_layout.tsx
@@ -1,16 +1,19 @@
-import { Stack } from 'expo-router/stack';
-
+// app/_layout.tsx
+import { Stack } from "expo-router/stack";
 import { ThemeProvider } from "../app/context/ThemeContext";
-
+// Import your TextScaleContext
+import { TextScaleProvider } from "../app/context/TextScaleContext";
 
 export default function Layout() {
   return (
     <ThemeProvider>
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="Pages/LoginPage" options={{headerShown: false}} />
-      <Stack.Screen name="Pages/RegisterPage" options={{headerShown: false}} />
-    </Stack>
+      <TextScaleProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="Pages/LoginPage" options={{ headerShown: false }} />
+          <Stack.Screen name="Pages/RegisterPage" options={{ headerShown: false }}/>
+        </Stack>
+      </TextScaleProvider>
     </ThemeProvider>
   );
 }
