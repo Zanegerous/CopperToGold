22bbabe9ac7e8716932b307abb908a7c713350a5
Lily <80413868+ndd323@users.noreply.github.com>
2025-02-16 21:49:46 -0600

    Add files via upload

---
+++ CTG/app/context/NotificationContext.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from "react"
+
+const NotificationContext = createContext({
+    isNotif: true,
+    toggleNotifications: () => {}
+  });
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [isNotif, setIsNotif] = useState(false);
+
+  const toggleNotifications = () => {
+    setIsNotif((prev) => !prev);
+  };
+
+  return (
+    <NotificationContext.Provider value={{ isNotif, toggleNotifications }}>
+      {children}
+    </NotificationContext.Provider>
+  );
+}
+
