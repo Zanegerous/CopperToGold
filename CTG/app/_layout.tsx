40adbfc5e7471c12c84c948f1600a8a2df9d44a9
Coby Taylor <117447744+KingOuroboros@users.noreply.github.com>
2025-02-23 20:12:10 -0600

    Sprint 3 Completed

--- CTG/app/_layout.tsx
+++ CTG/app/_layout.tsx
@@ -1,18 +1,24 @@
-// app/_layout.tsx
+import React from "react";
 import { Stack } from "expo-router/stack";
 import { ThemeProvider } from "../app/context/ThemeContext";
-// Import your TextScaleContext
 import { TextScaleProvider } from "../app/context/TextScaleContext";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
+
+import "./i18n";
 
 export default function Layout() {
   const [fontsLoaded, error] = useFonts({
-    "Lato-Black" : require("../assets/fonts/Lato-Black.ttf"),
-    "Lato-Bold" : require("../assets/fonts/Lato-Bold.ttf"),
-    "Lato-Light" : require("../assets/fonts/Lato-Light.ttf"),
-    "Lato-Regular" : require("../assets/fonts/Lato-Regular.ttf"),
-    "Lato-Thin" : require("../assets/fonts/Lato-Thin.ttf"),
-  })
+    "Lato-Black": require("../assets/fonts/Lato-Black.ttf"),
+    "Lato-Bold": require("../assets/fonts/Lato-Bold.ttf"),
+    "Lato-Light": require("../assets/fonts/Lato-Light.ttf"),
+    "Lato-Regular": require("../assets/fonts/Lato-Regular.ttf"),
+    "Lato-Thin": require("../assets/fonts/Lato-Thin.ttf"),
+  });
+
+  // If fonts haven't loaded yet, you can return a loading indicator or null.
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <ThemeProvider>
@@ -20,7 +26,7 @@ export default function Layout() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="Pages/LoginPage" options={{ headerShown: false }} />
-          <Stack.Screen name="Pages/RegisterPage" options={{ headerShown: false }}/>
+          <Stack.Screen name="Pages/RegisterPage" options={{ headerShown: false }} />
         </Stack>
       </TextScaleProvider>
     </ThemeProvider>
